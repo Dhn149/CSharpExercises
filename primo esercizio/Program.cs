b0d79caea3837b547a33f3ccdcfb7935fcef33fd
Dhn149 <marcocamici1986@gmail.com>
2020-11-29 20:10:13 +0100

    upload project files

--- primo esercizio/Program.cs
+++ primo esercizio/Program.cs
@@ -6,7 +6,15 @@ namespace primo_esercizio
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            /* primo esercizio svolto 
+            Console.WriteLine("Please, insert your name");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Your name is {name.ToUpper()}");
+            */
+
+            string catchArgs = args[0];
+            var args1 = catchArgs.ToUpper();
+            System.Console.WriteLine($"This is my argument to upper {args1}");
         }
     }
 }
